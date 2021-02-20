8aeb0de293884054f07ad67510ed0879467b6af2
Collin Town <tntcommando1@gmail.com>
2021-02-20 17:28:35 -0500

    Added toasts and refactored render.js

--- client/render.js
+++ client/render.js
@@ -1,4 +1,10 @@
+var timeScale = 60; // how many of config.scales.xAxes.time.unit to count
+var slider;
+var bubble;
 var chart;
+var successToast;
+var errorToast;
+
 var config = {
     type: 'line',
     data: {
@@ -13,8 +19,11 @@ var config = {
         responsive: true,
         maintainAspectRatio: false,
         title: {
-            display: true,
+            display: false,
             text: 'Moisture Reading'
+        },
+        legend: {
+            display: false
         },
         scales: {
             xAxes: [{
@@ -43,61 +52,67 @@ var config = {
     }
 }
 
-const slider = document.getElementById('timeSelector');
-const bubble = document.getElementById("timeSelectorBubble"); //bubbles stolen from https://css-tricks.com/value-bubbles-for-range-inputs/
-var timeScale = 60; // how many of config.scales.xAxes.time.unit to count
+window.addEventListener('load', (event) => {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    bubble = document.getElementById("timeSelectorBubble"); //bubbles stolen from https://css-tricks.com/value-bubbles-for-range-inputs/
 
-onload();
-initSlider();
-
-function onload() {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    chart = new Chart(ctx, config);
-};
-
-async function initSlider() {
-    
+    slider = document.getElementById('timeSelector');
     slider.addEventListener("input", async () => {
         timeScale = slider.value;
-        setBubble(slider, bubble);
-    });
-      
-    function setBubble(range, bubble) {
-        const val = range.value;
-        const min = range.min ? range.min : 0;
-        const max = range.max ? range.max : 100;
+        
+        const val = slider.value;
+        const min = slider.min ? slider.min : 0;
+        const max = slider.max ? slider.max : 100;
         const newVal = Number(((val - min) * 100) / (max - min));
         bubble.innerHTML = val;
         
         // Sorta magic numbers based on size of the native UI thumb
         bubble.style.left = newVal + "%";
-    }
-}
-
-
-
-webSocket = new WebSocket(`ws://${window.location.hostname}:443`);
-webSocket.onopen = function() {
-    console.log('Connection opened');
-};
-webSocket.onmessage = function(event) {
-    json = JSON.parse(event.data);
-    stat = parseInt(json.data);
-    time = new Date(json.time);
-
-    config.data.datasets[0].data.push({
-        x: time,
-        y: stat
     });
-   
+
+    chart = new Chart(ctx, config);
+
+    // Create the Toast objects
+    successToast = new bootstrap.Toast(document.getElementById('successToast'), {
+        animation: true,
+        autoHide: true,
+        delay: 2000
+    });
+    errorToast = new bootstrap.Toast(document.getElementById('errorToast'), {
+        animation: true
+    });
     
-    if (((time - config.data.datasets[0].data[0].x).valueOf() / 1000) > timeScale) {
-    //if (config.data.datasets[0].data.length > 60) {
-        config.data.datasets[0].data.shift();
-    //}
+    // Connect to the WebSocket and create listeners
+    connect();
+});
+
+function connect() {
+    webSocket = new WebSocket(`ws://${window.location.hostname}:443`);
+
+    webSocket.onopen = function() {
+        successToast.show();
+        console.log('Connection opened');
+    };
+    
+    webSocket.onmessage = function(event) {
+        json = JSON.parse(event.data);
+        stat = parseInt(json.data);
+        time = new Date(json.time);
+    
+        // Push data into the config data then update the chart with the new config
+        config.data.datasets[0].data.push({
+            x: time,
+            y: stat
+        });
+        
+        if (((time - config.data.datasets[0].data[0].x).valueOf() / 1000) > timeScale) {
+            config.data.datasets[0].data.shift();
+        }
+        chart.update();
+    };
+    
+    webSocket.onclose = function() {
+        errorToast.show();
+        console.log('Connection closed');
     }
-    chart.update();
-};
-webSocket.onclose = function() {
-    console.log('Connection closed');
 }
