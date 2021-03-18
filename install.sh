fd567a939dbeaeca229e05f95530ab4aa9111b3e
Darton <dartonaili@gmail.com>
2021-03-18 18:56:47 -0400

    Installer works better now that it fetches npm packages.

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 
 #sudo apt update
 #sudo apt install python3
+cd readlink -f $(dirname "$0")
 sudo python3 ./install/install.py
 chmod 775 start.sh
 chmod 775 ./NodePortableForPi/bin/node
-cp install/PlantWaterer.desktop ~/.config/autostart
+
+./NodePortableForPi/bin/npm i
+
